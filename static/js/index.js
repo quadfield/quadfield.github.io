5435eb1c0d6e4936bfcd2b409739cf2a84b8c50e
Gopal Sharma <gopalsharma1193@gmail.com>
2023-11-30 14:14:04 -0800

    image comparison

--- static/js/index.js
+++ static/js/index.js
@@ -3,14 +3,14 @@ window.HELP_IMPROVE_VIDEOJS = false;
 var INTERP_BASE = "./static/interpolation/stacked";
 var NUM_INTERP_FRAMES = 240;
 
-var interp_images = [];
-function preloadInterpolationImages() {
-  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-    interp_images[i] = new Image();
-    interp_images[i].src = path;
-  }
-}
+// var interp_images = [];
+// function preloadInterpolationImages() {
+//   for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
+//     var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
+//     interp_images[i] = new Image();
+//     interp_images[i].src = path;
+//   }
+// }
 
 function setInterpolationImage(i) {
   var image = interp_images[i];
@@ -65,13 +65,13 @@ $(document).ready(function() {
         player.currentTime = player.duration / 100 * this.value;
       })
     }, false);*/
-    preloadInterpolationImages();
+    // preloadInterpolationImages();
 
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+    // $('#interpolation-slider').on('input', function(event) {
+    //   setInterpolationImage(this.value);
+    // });
+    // setInterpolationImage(0);
+    // $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
 
     bulmaSlider.attach();
 
